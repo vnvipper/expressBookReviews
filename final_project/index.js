245a6e1266a0bea7b1fd8e9bb8a6460dd6f0644a
nguyenvpham <nguyenvpham@kms.com.vn>
2024-06-14 15:23:17 +0700

    Task 1

--- final_project/index.js
+++ final_project/index.js
@@ -8,15 +8,30 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    let token = req.headers["authorization"];
+    if (token) {
+        token = token.split(" ")[1];
+        jwt.verify(token, "access", (err, customer) => {
+            if (!err) {
+                req.customer = customer;
+                next();
+            }
+            else {
+                return res.status(403).json({ message: "Customer not authenticated!" })
+            }
+        });
+    }
+    else {
+        return res.status(403).json({ message: "Invalid token!" })
+    }
 });
- 
-const PORT =5000;
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
