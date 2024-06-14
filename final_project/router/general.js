4bbe090e569affd0e12033abb549f5b30b3cdf9f
nguyenvpham <nguyenvpham@kms.com.vn>
2024-06-14 16:00:13 +0700

    Complete task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -48,8 +48,8 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  res.send(books[isbn]?.reviews);
 });
 
 module.exports.general = public_users;
