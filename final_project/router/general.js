3164f9933b99156b541523da4f9e53e81f8ac2c9
nguyenvpham <nguyenvpham@kms.com.vn>
2024-06-14 15:53:01 +0700

    Complete task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,13 +19,20 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   const isbn = req.params.isbn;
-  res.send(books[isbn])
+  res.send(books[isbn]);
 });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let isbn;
+    for (const [id, book] of Object.entries(books)) {
+      console.log(book);
+        if (book.author === author) {
+            isbn = id;
+        }
+    }
+    res.send(books[isbn]);
 });
 
 // Get all books based on title
