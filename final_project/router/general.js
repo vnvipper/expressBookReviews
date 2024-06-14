d0320dae9138d56e57772861f644f9c1d459f3e7
nguyenvpham <nguyenvpham@kms.com.vn>
2024-06-14 16:10:47 +0700

    Complete task 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,7 +7,17 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+    if (!doesExist(username)) {
+      users.push({"username":username,"password":password});
+      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    } else {
+      return res.status(200).json({message: "User already exists!"});
+    }
+  }
+  return res.status(400).json({message: "username OR password are not provided."});
 });
 
 // Get the book list available in the shop
@@ -51,5 +61,16 @@ public_users.get('/review/:isbn',function (req, res) {
   const isbn = req.params.isbn;
   res.send(books[isbn]?.reviews);
 });
+
+const doesExist = (username)=>{
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports.general = public_users;
